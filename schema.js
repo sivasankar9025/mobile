7be3eafaa214a7a7bd1d2b1fe1b1d8ea58aec939
sivasankar9025 <pssm9025528322@gmail.com>
2024-08-16 23:11:18 +0530

    Add files via upload

---
+++ schema.js
@@ -0,0 +1,36 @@
+const mongoose=require("mongoose")
+
+const schema=new mongoose.Schema({
+    name:{
+        type:String,
+    },
+    registerNo:{
+        type:String,
+    },
+    gender:{
+        type:String,
+    },
+    graduate:{
+        type:String,
+    },
+    hsc:{
+        type:String,
+    },
+    myambition:{
+        type:String,
+    },
+    dept:{
+        type:String,
+    },
+    dob:{
+        type:String,
+    },
+    date:{
+        type:String
+    },
+    arr:[]
+    
+})
+const model=mongoose.model("model",schema)
+
+module.exports={model}
